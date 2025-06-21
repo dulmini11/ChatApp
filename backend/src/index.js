05170b7515c7474c12b06af0f2f017df692e2917
dulmini11 <dulminihw2@gmail.com>
2025-06-21 22:57:15 +0530

    Backend Setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,10 @@
-import express from "express"
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
 
-import authRoutes from "./routes/auth.route.js"
+const app = express();
 
-const app = express()
+app.use("/api/auth", authRoutes); // Or just "/auth" if preferred
 
-app.use("/api/auth",authRoutes)
-
-app.listen(5001, () =>{
-    console.log("server is runing on port 5001");
+app.listen(5001, () => {
+    console.log("Server is running on port 5001");
 });
