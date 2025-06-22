84964930bef8a4b52d58fc508c8e51244d6f0717
dulmini11 <dulminihw2@gmail.com>
2025-06-22 20:30:02 +0530

    Add update profile and check auth routes with cloudinary upload and auth middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,6 @@
-// src/routes/auth.route.js
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -8,4 +8,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
+router.put("/update-profile", protectRoute, updateProfile); 
+
+router.get("/check".protectRoute, updateProfile)
 export default router;
