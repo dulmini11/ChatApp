7772e4dfe6364ca5ed277989c3e9eb7eea356067
dulmini11 <dulminihw2@gmail.com>
2025-06-23 14:50:42 +0530

    Add messaging functionality with real-time support

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
